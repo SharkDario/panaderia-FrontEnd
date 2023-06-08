fccc3ff12960620720233cc14cbb4ddb397269f4
Miguel Dario Coronel <116774936+SharkDario@users.noreply.github.com>
2023-06-08 13:23:38 -0300

    Update formularioCompras.py

--- formularioCompras.py
+++ formularioCompras.py
@@ -80,17 +80,15 @@ class FormularioCompras:
         self.style = ttk.Style(self.cuaderno1)
         self.style.theme_use(self.tema)
         
-        #SI: blue, smog, black, adapta
-        #EH: kroc, plastik, winxpblue, itft1, aquativo, clam
-        #NO: equilux, keramik, elegance, radiance, breeze, clearlooks, ubuntu, yaru, scidmint
-        #[ 'alt', 'scidpurple', 'scidpink', 
-        #'default', 'scidblue', 'classic', 'xpnative', 
-        #, 'scidgrey', 'scidsand', 'scidgreen', 'arc', 'vista', 'winnative']
-
+        # pagina para registrar un proveedor
         self.registrarProveedor()
+        # posibilidad de ver y eliminar un proveedor mediante un combobox
         self.proveedorVerEliminar()
+        # para cargar un remito (aumentara el stock de cada materia prima que se agregue al detalle)
         self.remito()
+        # muestra la lista de las materias primas
         self.materiaPrima()
+        # muestra la lista de las materias primas con stock minimo
         self.materiaPrimaSM()
         self.cuaderno1.grid(column=1, row=1, padx=10, pady=10)
         self.ventana33.mainloop()
@@ -98,17 +96,17 @@ class FormularioCompras:
    
 
     def volverFormularioUsuario(self):
-        self.ventana3.deiconify()
-        self.ventana33.destroy()
-
+        self.ventana3.deiconify() # reaparece la ventana3
+        self.ventana33.destroy() # se destruye la ventana33
+    
     def proveedorVerEliminar(self):
         self.pagina5 = ttk.Frame(self.cuaderno1)
         self.pagina5.config(width=800, height=750)
         self.cuaderno1.add(self.pagina5, text="Ver Proveedor")
-
+        
         self.labelframe5 = labelF(self.pagina5, text="Información de Proveedor", font=(self.fuente, 20), fg=self.fuenteB, background=self.back)
         self.labelframe5.grid(column=0, row=0, padx=5, pady=10)
-
+        # se recuperan los nombres y dnis de los proveedores
         self.listaNom2 = Proveedor.recuperarNombres()
         self.listaDNI2 = Proveedor.recuperarDNIs()
 
@@ -124,43 +122,58 @@ class FormularioCompras:
         
         self.labelProve2 = label(self.labelframe5, text="Proveedor:", font=(self.fuente, 20), fg=self.fuenteB, background=self.back)
         self.labelProve2.grid(column=0, row=1, padx=4, pady=4)
-
+        # mediante el comboProv2 se puede seleccionar el nombre del proveedor
         self.comboProv2 = ttk.Combobox(self.labelframe5, font=(self.fuente, 20), width = 15, values=self.listaNom2)
         # Adding combobox drop down list
         self.comboProv2.set(self.listaNom2[0])
         self.comboProv2.grid(column = 1, row = 1)
+        # la funcion on_selectP2 provoca que se muestren los datos del proveedor seleccionado y a su vez se puede darlo de baja
         self.comboProv2.bind("<<ComboboxSelected>>", self.on_selectP2)
         
-        self.textProv2 = self.listaDNI2[0]
+        self.textProv2 = self.listaDNI2[0] #el primer dni de la lista para ponerlo en el labelProv2
         self.labelProv2 = label(self.labelframe5, text=f"DNI: {self.textProv2}", font=(self.fuente, 20), fg=self.fuenteB, background=self.back)
         self.labelProv2.grid(column=2, row=1, padx=4, pady=4)
-
+        # este sera el scrolledtext donde se insertaran los datos del proveedor
         self.scrolledtext2 = st.ScrolledText(self.labelframe5, font=(self.fuente, 20), width=20, height=10)
         self.scrolledtext2.grid(column=0, row=2, padx=10, pady=10)
         self.on_selectP2()
-
+        # boton que podra eliminar al proveedor
         self.botonEliminar = bt(self.labelframe5, text="Dar de baja", font=(self.fuente, 20), fg=self.fuenteB, background=self.backB, command=self.bajaProveedor)
         self.botonEliminar.grid(column=1, row=2, padx=4, pady=4)
-
+    
     def bajaProveedor(self):
+        # se saca el indice del nombre de proveedor seleccionado
         indice = self.comboProv2.current()
+        # se guarda el dni de ese proveedor
         dniEli = self.listaDNI2[indice]
+        # se elimina de la lista de nombres el proveedor
         self.listaNom2.pop(indice)
+        # tambien se elimina el dni de ese proveedor de la lista de dnis
         self.listaDNI2.remove(dniEli) 
+        # se configura el comboProv2 para que guarde la listaNom2 actualizada sin el nombre del proveedor eliminado
         self.comboProv2.config(values=self.listaNom2)
+        # se obtiene el id del proveedor guardado en la bd
         idProv = Proveedor.obtenerId((dniEli, ))
+        # se da de baja al proveedor
         Proveedor.bajaProveedor(idProv)
         mb.showinfo("¡Felicidades!", "Proveedor dado de baja")
 
     def on_selectP2(self, event=None):
+        # se saca el indice del comboProv2
         indice = self.comboProv2.current()
+        # se configura el scrolledtext2 para que se pueda escribir sobre el
         self.scrolledtext2.config(state="normal")
+        # se configura el labelProv2 para que muestre el dni del proveedor seleccionado
         self.textProv2 = self.listaDNI2[indice]
         self.labelProv2.config(text=f"DNI: {self.textProv2}")
+        # se obtiene el proveedor mediante el dni
         prove1 = Proveedor.obtenerProveedor(int(self.textProv2))
+        # se guarda en mensaje los datos del proveedor
         mensaje = f"DNI: {prove1.DNI}\nCUIL/CUIT: {prove1.CUIL_CUIT}\nNombre: {prove1.nombre}\nDomicilio: {prove1.domicilio}\nTeléfono: {prove1.telefono}\n"
         self.scrolledtext2.delete("1.0", tk.END)
+        # se inserta el mensaje en el scrolledtext2
         self.scrolledtext2.insert(tk.END, mensaje)
+        # se configura para desactivar la edicion
         self.scrolledtext2.config(state="disabled")
 
     def registrarProveedor(self):
@@ -173,53 +186,50 @@ class FormularioCompras:
         self.labelframe1 = labelF(self.pagina1, text="Registrar Proveedor", font=(self.fuente, 20), fg=self.fuenteB, background=self.back)
         self.labelframe1.grid(column=0, row=0, padx=5, pady=10)
 
-        #self.labelFecha = label(self.labelframe1, text='Fecha de contratación:', font=(self.fuente, 20), fg=self.fuenteB, background=self.back)
-        #self.labelFecha.grid(column=0, row=0, padx=4, pady=4)
-        #self.entradaFecha = DateEntry(self.labelframe1, font=(self.fuente, 20), fg=self.fuenteB, width=15, background=self.backB, foreground='white', borderwidth=2)
-        #self.entradaFecha.grid(column=1, row=0, padx=4, pady=4)
-
+        # label y entrada de DNI
         self.labelDNI = label(self.labelframe1, text="DNI:", font=(self.fuente, 20), fg=self.fuenteB, background=self.back)
         self.labelDNI.grid(column=0, row=1, padx=4, pady=4)
         self.entradaDNI = entry(self.labelframe1, font=(self.fuente, 20), fg=self.fuenteB)
         self.entradaDNI.grid(column=1, row=1, padx=4, pady=4)
-
+        # label y entrada de CUIL
         self.labelCUIL = label(self.labelframe1, text="CUIL/CUIT:", font=(self.fuente, 20), fg=self.fuenteB, background=self.back)
         self.labelCUIL.grid(column=0, row=2, padx=4, pady=4)
         self.entradaCUIL = entry(self.labelframe1, font=(self.fuente, 20), fg=self.fuenteB)
         self.entradaCUIL.grid(column=1, row=2, padx=4, pady=4)
-
+        #label y entrada de Nombre
         self.labelNombre = label(self.labelframe1, text="Nombre:", font=(self.fuente, 20), fg=self.fuenteB, background=self.back)
         self.labelNombre.grid(column=0, row=3, padx=4, pady=4)
         self.entradaNombre = entry(self.labelframe1, font=(self.fuente, 20), fg=self.fuenteB)
         self.entradaNombre.grid(column=1, row=3, padx=4, pady=4)
-
+        #label y entrada de domicilio
         self.labelDomi = label(self.labelframe1, text="Domicilio:", font=(self.fuente, 20), fg=self.fuenteB, background=self.back)
         self.labelDomi.grid(column=0, row=4, padx=4, pady=4)
         self.entradaDomi = entry(self.labelframe1, font=(self.fuente, 20), fg=self.fuenteB)
         self.entradaDomi.grid(column=1, row=4, padx=4, pady=4)
-
+        #label y entrada de telefono
         self.labelTel = label(self.labelframe1, text="Teléfono:", font=(self.fuente, 20), fg=self.fuenteB, background=self.back)
         self.labelTel.grid(column=0, row=5, padx=4, pady=4)
         self.entradaTel = entry(self.labelframe1, font=(self.fuente, 20), fg=self.fuenteB)
         self.entradaTel.grid(column=1, row=5, padx=4, pady=4)
-
+        # boton confirmar para dar de alta a un nuevo proveedor
         self.botonConfirmar = bt(self.labelframe1, text="Registrar", font=(self.fuente, 20), fg=self.fuenteB, background=self.backB, command=self.altaNuevoProveedor)
         self.botonConfirmar.grid(column=1, row=10, padx=4, pady=4)
         
 
     def altaNuevoProveedor(self):
-
-        dni = f.ingDNI_CUIL(self.entradaDNI.get(), 8, "DNI")
-        cuil = f.ingDNI_CUIL(self.entradaCUIL.get(), 11)
-        nombre = self.entradaNombre.get()
-        domi = self.entradaDomi.get()
-        tel = f.ingNum(self.entradaTel.get(), "El teléfono", 15)
+        # se valida que haya ingresado datos correctos para cada variable
+        dni = f.ingDNI_CUIL(self.entradaDNI.get(), 8, "DNI") # 8 digitos y entero
+        cuil = f.ingDNI_CUIL(self.entradaCUIL.get(), 11) # 11 digitos y entero
+        nombre = self.entradaNombre.get() 
+        domi = self.entradaDomi.get() 
+        tel = f.ingNum(self.entradaTel.get(), "El teléfono", 15) #entero y 15 digitos
 
         #fecha = datetime.strptime(self.entradaFecha.get(), '%m/%d/%y') #datetime.strptime("5-26-2023", '%m-%d-%Y')
         #Esto es para verificar si el DNI ya existe en las tablas de Proveedores
         idProveedor = Proveedor.obtenerId((dni,))
-        
+        # esto es para comparar cuil y dni, verificar si estos coinciden
         dniCuilComparar = f.dniCuilComparar(dni, cuil)
+        # si todos los entrys dan True, entonces dara de alta un nuevo proveedor
         entrysValidos = ((nombre!="")&(domi!="")&(dniCuilComparar)&(type(tel) is not list)&(idProveedor==-1))
         if(entrysValidos):
             #nombreProveedor, CUIL_CUIT, DNI, domicilioProveedor, telefonoProveedor
@@ -227,8 +237,8 @@ class FormularioCompras:
             mb.showinfo("¡Felicidades!", "Proveedor registrado")
         else:
             mensaje=""
-            #print(dni)
-            if(type(dni) is list):
+            #las variables son listas con un solo elemento ["mensaje"] si es que se ingreso un dato no valido
+            if(type(dni) is list): # dni e
                 mensaje += dni[0]
             if(type(cuil) is list):
                 mensaje += "\n"+cuil[0]
@@ -244,6 +254,7 @@ class FormularioCompras:
                 mensaje += "\nEl DNI ya existe en la base de datos."
             #print(mensaje)
             mb.showerror("Error", mensaje)
+        # se eliminan de las entradas todo lo escrito
         self.entradaDNI.delete(0, tk.END)
         self.entradaCUIL.delete(0, tk.END)
         self.entradaNombre.delete(0, tk.END)
